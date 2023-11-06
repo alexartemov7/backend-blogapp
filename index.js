acee3976a56447b8603b79220aff444b1f6793f0
Alex Artemov <artemov.sandro@gmail.com>
2023-11-06 16:26:33 -0500

    backend for the blogapp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from 'express';
+import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
+
 import 'dotenv/config'
 
 const app = express()
@@ -8,24 +9,40 @@ app.use(cors())
 app.use(express.json())
 
 const client = new MongoClient(process.env.MONGO_URI)
-const db = client.db('blogapp-c12-aa')
+const db = client.db('blogapp-c12')
 const blogPosts = db.collection('blog-posts')
+const usersDb = db.collection('users')
 
 client.connect()
 console.log('Connected to Mongo')
 
 app.get('/', async (req, res) => {
-    const allPosts = await blogPosts.find().toArray()
-    console.log('allPosts -> ', allPosts)
-    res.send(allPosts)
+	const allPosts = await blogPosts.find().toArray()
+	console.log('allPosts -> ', allPosts)
+	res.send(allPosts)
 })
+
 app.post('/', async (req, res) => {
-    const newBlogPost = {title: req.body.title, content: req.body.content}
-    const addedItem = await blogPosts.insertOne(newBlogPost)
+	const newBlogPost = { title: req.body.title, content: req.body.content }
+	await blogPosts.insertOne(newBlogPost)
 
-    const allPosts = await blogPosts.find().toArray()
-    res.send(addedItem)
+	const allPosts = await blogPosts.find().toArray()
+	res.send(allPosts)
 })
 
+// sign up
+app.post('/signup', async (req, res) => {
+	const userAdded = await usersDb.insertOne({ email: req.body.email, password: req.body.password })
+	console.log('user added -> ', userAdded)
+	res.send(userAdded)
+})
+
+// log in
+app.post('/login', async (req, res) => {
+	console.log(req.body)
+    const userFound = await usersDb.findOne({email: req.body.email})
+
+    res.send(userFound)
+})
 
 app.listen('8080', () => console.log('Api listening on port 8080 😎'))
